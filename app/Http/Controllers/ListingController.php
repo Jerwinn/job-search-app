68bd65ba604cb68bcabcf9a1287b6699982550cc
Jerwinn <Jerwin@live.co.uk>
2022-12-16 00:19:56 +0000

    latest

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ListingController extends Controller
 {
-    //
+    //Show all job listings
+   public function index() {
+        return view('listings', [
+            'listings' => Listing::all()
+        ]);
+    }
+    //Show single job listings 
+    public function show(Listing $listing) {
+        return view('listing', [
+            'listing' => $listing
+        ]);
+    }
 }
