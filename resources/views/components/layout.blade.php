f81a66a07b341af43b038dde7b4a2a52e2024c2c
Jerwinn <Jerwin@live.co.uk>
2023-01-04 22:34:39 +0000

    working on login functionality

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -30,12 +30,28 @@
   <nav class="flex justify-between items-center mb-4">
     <a href="/"><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo" /></a>
     <ul class="flex space-x-6 mr-6 text-lg">
+      @auth
       <li>
-        <a href="register.html" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
+        <span class="font-bold uppercase"></span>
+          Welcome {{auth()->user()->name}}
       </li>
       <li>
-        <a href="login.html" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i> Login</a>
+        <a href="listings/manage" class="hover:text-laravel"><i class="fa fa-gear"></i> Manage Listing</a>
       </li>
+        <form class="inline" method="POST" action="/logout">
+        @csrf
+        <button type="submit">
+          <i class="fa-solid fa-door-closed"></i> Logout
+        </button>
+      </form>
+      <li>
+      @else
+        <a href="/register" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
+      </li>
+      <li>
+        <a href="login" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i> Login</a>
+      </li>
+      @endauth
     </ul>
   </nav>
 
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\ListingController;
 use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -40,3 +41,17 @@ Route::get(
     [ListingController::class, 'show']
 );
 
+// Show Register Form
+Route::get('/register', [UserController::class, 'create']);
+
+//Create New User
+Route::post('/users', [UserController::class, 'store']);
+
+//Logout User
+Route::post('/logout', [UserController::class, 'logout']);
+
+//Show login form
+Route::get('/login', [UserController::class, 'login']);
+
+//Login User
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
