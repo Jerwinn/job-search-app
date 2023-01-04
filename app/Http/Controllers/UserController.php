0ac6eeebc30b642c835b51327e5d66ebe18b5bfb
Jerwinn <Jerwin@live.co.uk>
2023-01-04 22:34:46 +0000

    working on login functionality

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Auth\User;
+
+class UserController extends Controller
+{
+    //Show Register/Create Form
+    public function create() {
+        return view('users.register');
+    }
+
+    //Create New User
+    public function store(Request $request) {
+        $formFields = $request->validate([
+            'name' => ['required', 'min:3'],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+         // Hash Password
+        $formFields['password'] = bcrypt($formFields['password']);
+
+        //create User
+        $user = User::create($formFields);
+
+        //Login
+        auth()->login($user);
+
+        return redirect('/')->with('message', 'User Created and Logged in');
+
+    }
+
+    //logout
+    public function logout(Request $request) {
+        auth()->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/')->with('message', 'You have been logged out');
+    }
+
+    //Show login form
+    public function login() {
+        return view('users.login');
+    }
+
+    public function authenticate(Request $request) {
+        $formFields = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => 'required'
+        ]);
+
+        if(auth()->attempt($formFields)){
+            
+        }
+    }
+
+}
