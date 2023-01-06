76d17f002479c561ba1e272a6d87c79e688ff297
Jerwinn <Jerwin@live.co.uk>
2023-01-06 01:28:44 +0000

    FInished login
    login functionality now working

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -56,8 +56,12 @@ class UserController extends Controller
         ]);
 
         if(auth()->attempt($formFields)){
-            
+            $request->session()->regenerate();
+
+            return redirect('/')->with('message', 'You are now logged in!');
         }
+        return back()->withErrors(['email' => 'Invalid Credentials'])
+        ->onlyInput('email');
     }
 
 }
